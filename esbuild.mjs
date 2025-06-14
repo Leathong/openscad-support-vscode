7cc69d8fcf2ce468198bd7564a05ad30b2acbcde
Jesse Ditson <jesse.ditson@gmail.com>
2025-06-14 13:09:00 -0700

    add esbuild for preview

---
+++ esbuild.mjs
@@ -0,0 +1,30 @@
+#!/usr/bin/env node
+
+import * as esbuild from 'esbuild'
+
+const watch = process.argv.includes("--watch");
+
+const buildContext = await esbuild.context({
+  entryPoints: ['preview-panel/main.ts'],
+  bundle: true,
+  minify: true,
+  sourcemap: true,
+  target: ['chrome58'],
+  outfile: 'out/preview/main.js',
+})
+
+if (watch) {
+  await buildContext.watch();
+
+//   const { host, port } = await buildContext.serve({
+//     servedir: "out/preview",
+//     port: 8888,
+//     cors: {
+//       origin: "*",
+//     },
+//   });
+//   console.log(`server started at http://${host}:${port}`);
+} else {
+  await buildContext.rebuild();
+  process.exit(0);
+}
